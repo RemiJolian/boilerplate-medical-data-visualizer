91a98b86edaa629bb5ceb782a89056b3dce7958c
RaminBagheri <rbagheri85@gmail.com>
2024-04-20 12:49:42 +0330

    hn

--- medical_data_visualizer.py
+++ medical_data_visualizer.py
@@ -33,23 +33,18 @@ df['gluc'] = np.where((df['gluc'] == 1), 0, 1)
 def draw_cat_plot():
     # Create DataFrame for cat plot using `pd.melt` using just the values from 'cholesterol',
     # 'gluc', 'smoke', 'alco', 'active', and 'overweight'.
-    df_cat = None
+    df_cat = pd.melt(df, id_vars =["cardio"], value_vars = ['cholesterol', 'gluc', 'smoke',
+    'alco','active', 'overweight'])
 
-
-    # Group and reformat the data to split it by 'cardio'. Show the counts of each feature.
-    # You will have to rename one of the columns for the catplot to work correctly.
-    df_cat = None
-    
+    df_cat["total"] = 1
+    df_cat = df_cat.groupby(["cardio", "variable", "value"], as_index = False).count()
 
     # Draw the catplot with 'sns.catplot()'
+    sns.set_theme(style="darkgrid")
+    fig = sns.catplot(x= "variable", y = "total", data = df_cat,hue = "value", kind = "bar",
+    col = "cardio").fig
 
-
-
-    # Get the figure for the output
-    fig = None
-
-
-    # Do not modify the next two lines
+     # Do not modify the next two lines
     fig.savefig('catplot.png')
     return fig
 
@@ -57,21 +52,27 @@ def draw_cat_plot():
 # Draw Heat Map
 def draw_heat_map():
     # Clean the data
-    df_heat = None
+    df_heat = df[
+        (df["ap_lo"] <= df["ap_hi"]) &
+        (df['height'] >= df['height'].quantile(0.025)) & 
+        (df['height'] <= df['height'].quantile(0.975)) & 
+        (df['weight'] >= df['weight'].quantile(0.025)) & 
+        (df['weight'] <= df['weight'].quantile(0.975))]
 
     # Calculate the correlation matrix
-    corr = None
+    corr = df_heat.corr(method = "pearson")
 
     # Generate a mask for the upper triangle
-    mask = None
+    mask = np.triu(corr)
 
 
 
     # Set up the matplotlib figure
-    fig, ax = None
+    fig, ax = plt.subplots(figsize=(12,12))
 
     # Draw the heatmap with 'sns.heatmap()'
-
+    sns.heatmap(corr, linewidths=1, annot = True, square = True, mask = mask, fmt = '.1f',
+                center = 0.08, cbar_kws = {"shrink":0.5})
 
 
     # Do not modify the next two lines
